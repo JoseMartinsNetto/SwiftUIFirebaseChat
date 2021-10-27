d46efed22f8233c0ea7e86c34ed1b5d094661d93
jmsantosnetto <j.msantos.netto@gmail.com>
2021-10-27 01:03:38 -0300

    Initial Commit

---
+++ SwiftUIFirebaseRealTimeChat/SwiftUIFirebaseRealTimeChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseRealTimeChatApp.swift
+//  SwiftUIFirebaseRealTimeChat
+//
+//  Created by Jose Martins on 27/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseRealTimeChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
