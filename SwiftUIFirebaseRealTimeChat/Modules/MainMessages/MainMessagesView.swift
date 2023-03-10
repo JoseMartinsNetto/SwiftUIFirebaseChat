00799c6f29be1da1c69bbe08f5cd76636a8892a4
José Martins <j.msantos.netto@gmail.com>
2023-03-10 20:47:49 -0300

    Add New Messages Button and Settings Action Sheet to MainMessagesView

--- SwiftUIFirebaseRealTimeChat/Modules/MainMessages/MainMessagesView.swift
+++ SwiftUIFirebaseRealTimeChat/Modules/MainMessages/MainMessagesView.swift
@@ -7,7 +7,62 @@
 
 import SwiftUI
 
+struct MainMessagesView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                CustomNavBar(onLogout: {
+                    print("handle logout")
+                })
+                
+                ScrollView {
+                    ForEach(0..<10) { index in
+                        VStack {
+                            HStack(spacing: 16) {
+                                Image(systemName: "person.fill")
+                                    .padding(8)
+                                    .font(.system(size: 32))
+                                    .overlay(RoundedRectangle(cornerRadius: 44)
+                                        .stroke(Color(.label), lineWidth: 1))
+                                
+                                VStack(alignment: .leading) {
+                                    Text("Username")
+                                        .font(.system(size: 14, weight: .bold))
+                                    Text("Message sent to user")
+                                        .font(.system(size: 14))
+                                        .foregroundColor(Color(.lightGray))
+                                }
+                                Spacer()
+                                
+                                Text("22d")
+                                    .font(.system(size: 14, weight: .semibold))
+                            }
+                            Divider()
+                                .padding(.vertical, 8)
+                        }.padding(.horizontal)
+                    }
+                }
+            }
+            .overlay(
+                NewMessagesButton {
+                    
+                }
+                , alignment: .bottom
+            )
+            .navigationBarHidden(true)
+        }
+    }
+}
+
 fileprivate struct CustomNavBar: View {
+    @State var sholdShowLogoutOptions = false
+    
+    let onLogout: () -> Void
+    
+    init(onLogout: @escaping () -> Void) {
+        self.onLogout = onLogout
+    }
+    
     var body: some View {
         HStack(spacing: 16) {
             Image(systemName: "person.fill")
@@ -28,47 +83,51 @@ fileprivate struct CustomNavBar: View {
             }
             
             Spacer()
-            Image(systemName: "gear")
             
-        }.padding()
+            Button {
+                sholdShowLogoutOptions.toggle()
+            } label: {
+                Image(systemName: "gear")
+                    .foregroundColor(Color.black)
+            }
+            
+        }
+        .padding()
+        .actionSheet(isPresented: $sholdShowLogoutOptions) {
+            .init(title: Text("Settings"),
+                  message: Text("What you want to do ?"),
+                  buttons: [
+                    .destructive(Text("Sing Out"), action: onLogout),
+                    .cancel()
+                  ]
+            )
+        }
     }
 }
 
-struct MainMessagesView: View {
+fileprivate struct NewMessagesButton: View {
+    let action: () -> Void
+    
+    init(action: @escaping () -> Void) {
+        self.action = action
+    }
+    
     var body: some View {
-        NavigationView {
-            VStack {
-                CustomNavBar()
-                
-                ScrollView {
-                    ForEach(0..<10) { index in
-                        VStack {
-                            HStack(spacing: 16) {
-                                Image(systemName: "person.fill")
-                                    .padding(8)
-                                    .font(.system(size: 32))
-                                    .overlay(RoundedRectangle(cornerRadius: 44)
-                                        .stroke(Color.black, lineWidth: 1))
-                                
-                                VStack(alignment: .leading) {
-                                    Text("Username")
-                                        .font(.system(size: 14, weight: .bold))
-                                    Text("Message sent to user")
-                                        .font(.system(size: 14))
-                                        .foregroundColor(Color(.lightGray))
-                                }
-                                Spacer()
-                                
-                                Text("22d")
-                                    .font(.system(size: 14, weight: .semibold))
-                            }
-                            Divider()
-                                .padding(.vertical, 8)
-                        }.padding(.horizontal)
-                    }
+            Button {
+                action()
+            } label: {
+                HStack {
+                    Spacer()
+                    Text("+ New Message")
+                    Spacer()
                 }
-            }.navigationBarHidden(true)
-        }
+                .foregroundColor(Color.white)
+                .padding(.vertical)
+                .background(Color.blue)
+                .cornerRadius(32)
+                .padding(.horizontal)
+                .shadow(radius: 5)
+            }
     }
 }
 
